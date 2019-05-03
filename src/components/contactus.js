11db4cc2f81cb57a913ce9722e196fc959fcd36e
Ivy Strickland <IvyStrickland@pursuit.org>
2019-05-03 16:56:57 -0400

    container

--- src/components/contactus.js
+++ src/components/contactus.js
@@ -3,12 +3,15 @@ import React from 'react';
 const Contact = (props) => {
     return(
         <>
+        <div className='container' >
         <h1>
             Email : ProsperousHooligans@gmail.com
             Phone : 123-123-1234
             IG : Prosperous.Hooligans
             Facebook : ProsperousHooligans
-        </h1>
+        </h1>            
+        </div>
+
         </>
     )
 }
